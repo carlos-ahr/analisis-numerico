53a152da8201c1bd70c3f024761ea36bc955b51d
carlos-ahr <89262680+carlos-ahr@users.noreply.github.com>
2022-09-02 11:32:08 -0500

    02/09/2022

--- metodo-biseccion.cpp
+++ metodo-biseccion.cpp
@@ -1,61 +1,81 @@
-#include <ios>
-#include <iostream>
-#include <cmath>
-
+#include<iostream>
+#include<cmath>
 using namespace std;
-
 class biseccion{
-    double xi, xf, er;
-    int n;
-public:
-    biseccion(double i, double f, double e, int num){xi=i, xf=f, er=e, n=num;};
-    double f(double x){return pow(x, 2)-2;};
-    void calc_raiz();
-    ~biseccion(){cout<<"Limpiando memoria..."<<endl;};
+	private:
+		double xi, xf, err, error;
+	public:
+		biseccion();
+		void leerDatos();
+		double f(double x){return pow(x, 2)-x-1;}
+		double m(double i, double f){return(i+f)/2;}
+		double e(double m, double i){return abs((m-i)/m)*100;}
+		void calcRaiz();
+		~biseccion();
 };
-
-void biseccion::calc_raiz(){
-    double xm=(xi+xf)/2;
-    int num=1;
-    cout<<"n    xi    xf    f(xi)    (xm)    f(xi)f(xf)    xm    f(xm)    Error"<<endl;
-    if(f(xi)*f(xf)<0){
-        do{
-            if (f(xi)*f(xm)==0) {
-            cout<<"La raiz es: "<<xm<<endl;
-            }
-            else if (f(xi)*f(xm)<0){
-                cout<<num<<" "
-                    <<xi<<" "
-                    <<xf<<" "
-                    <<f(xi)<<" "
-                    <<f(xf)<<" "
-                    <<f(xi)*f(xf)<<" "
-                    <<xm<<" "
-                    <<f(xm)<<" "
-                    <<f(xi)/f(xm)<<" "
-                    <<fabs((xi-xm)/xm*100)<<endl;
-                xf=xm;
-            }
-            else if (f(xi)*f(xm)>0){
-                cout<<num<<"\t"
-                    <<xi<<"\t"
-                    <<xf<<"\t"
-                    <<f(xi)<<"\t"
-                    <<f(xf)<<"\t"
-                    <<f(xi)*f(xf)<<"\t"
-                    <<xm<<"\t"
-                    <<f(xm)<<"\t"
-                    <<f(xi)/f(xm)<<"\t"
-                    <<fabs((xi-xm)/xm*100)<<endl;
-                xi=xm;
-            }
-        }   
-        while(fabs((xi-xm)/xm*100)<er);
-    }
+biseccion::biseccion(){
+	xi=0.0;
+	xf=0.0;
+	err=0.0;
+	error=0.0;
 }
-
-int main(){
-    biseccion raiz(-2, -1, 1, 5);
-    raiz.calc_raiz();
-    return 0;
+void biseccion::leerDatos(){
+	cout<<"Ingrese el valor inicial:"<<endl;
+	cin>>xi;
+	cout<<"Ingrese el valor final:"<<endl;
+	cin>>xf;
+	cout<<"Ingrese el error: "<<endl;
+	cin>>err;
+	cout<<endl;
+}
+void biseccion::calcRaiz(){
+	do{
+		double fxi=f(xi);
+		double fxf=f(xf);
+		if(fxi*fxf<0){
+			double xm=m(xi, xf);
+			double fxm=f(xm);
+			error=e(xm, xi);
+			if(fxi*fxm==0){
+				cout<<"La raiz es: "<<xm<<endl;
+				break;
+			}
+			else if(fxi*fxm<0){
+				cout<<"Xi: "<<xi<<endl;
+				cout<<"Xf: "<<xf<<endl;
+				cout<<"F(Xi): "<<fxi<<endl;
+				cout<<"F(Xf): "<<fxf<<endl;
+				cout<<"F(Xi)F(Xf): "<<fxi*fxf<<endl;
+				cout<<"Xm: "<<xm<<endl;
+				cout<<"F(Xm): "<<fxm<<endl;
+				cout<<"F(Xi)F(Xm): "<<fxi*fxm<<endl;
+				cout<<"Error: "<<error<<"%"<<endl<<endl;
+				xf=xm;
+			}
+			else if(fxi*fxm>0){
+				cout<<"Xi: "<<xi<<endl;
+				cout<<"Xf: "<<xf<<endl;
+				cout<<"F(Xi): "<<fxi<<endl;
+				cout<<"F(Xf): "<<fxf<<endl;
+				cout<<"F(Xi)F(Xf): "<<fxi*fxf<<endl;
+				cout<<"Xm: "<<xm<<endl;
+				cout<<"F(Xm): "<<fxm<<endl;
+				cout<<"F(Xi)F(Xm): "<<fxi*fxm<<endl;
+				cout<<"Error: "<<error<<"%"<<endl<<endl;
+				xi=xm;	
+			}
+		}
+		else{
+			cout<<"Intervalo invalido :c"<<endl;
+		}
+	}while(error>err);
+}
+biseccion::~biseccion(){
+	cout<<"Limpiando memoria..."<<endl;
+}
+int main(void){
+	biseccion r1;
+	r1.leerDatos();
+	r1.calcRaiz();
+	return 0;
 }
